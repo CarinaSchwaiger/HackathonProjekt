7708c6addd988904679fb008d465d646b862bf37
bambampham <48788391+bambampham@users.noreply.github.com>
2020-10-10 17:14:09 +0200

    Apps - Pham

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Component from './Component';
+import DiscreteSlider from './Slider.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Component/>
+        <DiscreteSlider/>
         <a
           className="App-link"
           href="https://reactjs.org"
