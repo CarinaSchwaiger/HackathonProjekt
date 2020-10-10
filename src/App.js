190604ae94f145b4ee5fcdf14c979909f62c983f
tania <luety@econda.de>
2020-10-10 17:23:39 +0200

    move import from body to top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import VideoComponent from './VideoComponent';
+import DiscreteSlider from './Slider.js'
 
 const testvideo = 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4'
 const posterurl = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn1.vectorstock.com%2Fi%2F1000x1000%2F95%2F85%2Funicorn-on-a-pink-background-with-stars-postcard-vector-27119585.jpg&imgrefurl=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Funicorn-on-a-pink-background-with-stars-postcard-vector-27119585&tbnid=GuqLC2Eo7qsfYM&vet=12ahUKEwiz7b-7qKrsAhWRIMUKHRKwDHcQMygAegUIARCfAg..i&docid=sbcM0B27QLrhZM&w=1000&h=1080&q=unicorn%20pic&client=safari&ved=2ahUKEwiz7b-7qKrsAhWRIMUKHRKwDHcQMygAegUIARCfAg'
-import DiscreteSlider from './Slider.js'
 
 function App() {
   return (
