88929c20a4312d0cd4d1399ccac22e6f8ff896f7
Manuel Lang <manuellang183@gmail.com>
2020-10-11 11:03:07 +0200

    Updates

---
+++ precache-manifest.c2d2b6999f7291c027d8e4834ed2b0eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aef95975c747d894540ff39deca4617",
+    "url": "/HackathonProjekt/index.html"
+  },
+  {
+    "revision": "6d7b1df3fcfa574a09fb",
+    "url": "/HackathonProjekt/static/css/2.a5277431.chunk.css"
+  },
+  {
+    "revision": "854f6b8917b1d110e435",
+    "url": "/HackathonProjekt/static/css/main.39ef33ff.chunk.css"
+  },
+  {
+    "revision": "6d7b1df3fcfa574a09fb",
+    "url": "/HackathonProjekt/static/js/2.dabac5b1.chunk.js"
+  },
+  {
+    "revision": "89317c859db555d0a2c8e7e2f99ae9e8",
+    "url": "/HackathonProjekt/static/js/2.dabac5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854f6b8917b1d110e435",
+    "url": "/HackathonProjekt/static/js/main.9b6f15e7.chunk.js"
+  },
+  {
+    "revision": "553fc94d1cdb90724723",
+    "url": "/HackathonProjekt/static/js/runtime-main.e24ea784.js"
+  },
+  {
+    "revision": "bc3646103f469d4e3f3eb93bd422ff5b",
+    "url": "/HackathonProjekt/static/media/pic.bc364610.jpg"
+  }
+]);
